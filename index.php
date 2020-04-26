766cff1aa5541f67d0d747cafb99e7e1ab99da2f
Cardozo01 <cardozo01@cardozogroup.com>
2020-04-25 19:49:38 -0500

    git commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sin título</title>
+</head>
+
+<body>
+<?php
+	print "Bienvenidos al curso PHP<br>";
+	print "Hola alumnos<br>";
+	print "Hasta el próximo video<br>";
+	$nombre1="Juan";
+	print "El nombre del usuario es:".$nombre1
+	
+?>
+</body>
+</html>
---
+++ video05.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sin título</title>
+</head>
+
+<body>
+	<?php
+	echo "Este el primer mensaje <br>";
+	echo "Este el segundo mensaje <br>";
+	
+	?>
+</body>
+</html>
